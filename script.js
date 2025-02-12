9682c9947693a58dfc26a9e63cdff478c7d941f5
ivan <ivanmangulabnan19@gmail.com>
2025-02-12 10:55:21 +0800

    Aligned variables to the module pattern

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function Cell() {
     return { addValue, getValue };
 }
 
-function GameBoard() {
+const GameBoard = (() => {
 
     const board = [];
 
@@ -32,11 +32,9 @@ function GameBoard() {
     }
 
     return { getBoard, putToken, printBoard };
-}
+})();
 
 function GameController(playerOne = `Ivan`, playerTwo = `Jim`) {
-
-    const board = GameBoard();
 
     const players = [
         { playerName: playerOne, token: `X` },
@@ -50,7 +48,7 @@ function GameController(playerOne = `Ivan`, playerTwo = `Jim`) {
     const switchPlayer = () => currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
 
     const intro = () => {
-        board.printBoard();
+        GameBoard.printBoard();
         console.log(`Welcome Game Start!`);
         console.log(`${getCurrentPlayer().playerName} will start`);
     }
@@ -59,27 +57,27 @@ function GameController(playerOne = `Ivan`, playerTwo = `Jim`) {
 
         const player = getCurrentPlayer();
 
-        const targetCell = board.getBoard()[row].find((_, colIndex) => colIndex === col);
+        const targetCell = GameBoard.getBoard()[row].find((_, colIndex) => colIndex === col);
 
-        const checkDraw = () => board.getBoard().every(item => item.every(cell => cell.getValue() !== ``));
+        const checkDraw = () => GameBoard.getBoard().every(item => item.every(cell => cell.getValue() !== ``));
 
         const checkWin = () => {
-            const length = board.getBoard().length;
+            const length = GameBoard.getBoard().length;
 
-            if (board.getBoard()[row].every(cell => cell.getValue() === player.token)) return true; // check row where the value was placed.
-            if (board.getBoard().every(item => item[col].getValue() === player.token)) return true; // check column where the value was placed.
-            if (row === col && board.getBoard().every((_, index) => board.getBoard()[index][index].getValue() === player.token)) return true; // check top-left to bottom-right 
-            if (row + col === length - 1 && board.getBoard().every((_, index) => board.getBoard()[index][length - 1 - index].getValue() === player.token)) return true // check top right to bottom left
+            if (GameBoard.getBoard()[row].every(cell => cell.getValue() === player.token)) return true; // check row where the value was placed.
+            if (GameBoard.getBoard().every(item => item[col].getValue() === player.token)) return true; // check column where the value was placed.
+            if (row === col && GameBoard.getBoard().every((_, index) => GameBoard.getBoard()[index][index].getValue() === player.token)) return true; // check top-left to bottom-right 
+            if (row + col === length - 1 && GameBoard.getBoard().every((_, index) => GameBoard.getBoard()[index][length - 1 - index].getValue() === player.token)) return true // check top right to bottom left
 
             return false;
         }
 
         if (targetCell.getValue() === ``) {
-            board.putToken(row, col, player.token);
+            GameBoard.putToken(row, col, player.token);
             if (!checkWin() && !checkDraw()) {
                 console.log(`${player.playerName} put his token in row ${row} and col ${col}`);
                 switchPlayer();
-                board.printBoard();
+                GameBoard.printBoard();
                 console.log(`${getCurrentPlayer().playerName}'s turn`);
             } else if (checkDraw()) {
                 console.log(`GAME OVER! It's DRAW`);
